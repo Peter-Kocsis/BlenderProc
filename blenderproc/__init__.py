8c8c46e27f0eec7b873ed331c226725fc2ce63f0
Peter Kocsis <peter.kocsis@tum.de>
2023-01-10 11:54:50 +0100

    Revert

--- blenderproc/__init__.py
+++ blenderproc/__init__.py
@@ -37,23 +37,5 @@ else:
     first_file_name = file_names_of_stack[0]
     # check if the name of this file is either blenderproc or if the "OUTSIDE_OF_THE_INTERNAL_BLENDER_PYTHON_ENVIRONMENT_BUT_IN_RUN_SCRIPT" is set, which is set in the cli.py
     is_correct_startup_script = first_file_name != "blenderproc" and first_file_name != "command_line.py"
-
-    def _call_command(command_str, logger=None):
-        import subprocess
-
-        if logger is not None:
-            logger.info(f"Calling {command_str}")
-
-        process = subprocess.Popen(command_str,
-                                  universal_newlines=True,
-                                  shell=True,
-                                  stdout=sys.stdout,
-                                  stderr=sys.stderr)
-
-        process.wait()
-        return process.returncode
-
     if "OUTSIDE_OF_THE_INTERNAL_BLENDER_PYTHON_ENVIRONMENT_BUT_IN_RUN_SCRIPT" not in os.environ and is_correct_startup_script:
-        print("Running script from BlenderProc!")
-        exit_code = _call_command(f"blenderproc run {' '.join(sys.argv)}")
-        sys.exit(exit_code)
+        raise Exception(f"\n###############\nThis script can only be run by \"blenderproc run\", instead of calling:\n\tpython {sys.argv[0]}\ncall:\n\tblenderproc run {sys.argv[0]}\n###############")
